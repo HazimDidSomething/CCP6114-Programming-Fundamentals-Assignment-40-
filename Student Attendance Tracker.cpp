9c3f59c619f266793bdc21350a40166220b957e8
Hazim Zubair <hazimzubair81@gmail.com>
2025-12-25 15:54:46 +0800

    test and adding the files

---
+++ Student Attendance Tracker.cpp
@@ -0,0 +1,17 @@
+// *********************************************************
+// Program: Student Attendance Tracker.cpp
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC1L
+// Tutorial Class: TT4L
+// Trimester: 2530
+// Member_1: 253UC246FX | Hazim Zubair | hazim.zubair1@students.mmu.edu.my
+// Member_2: ID | NAME | EMAIL
+// Member_3: ID | NAME | EMAIL
+// Member_4: ID | NAME | EMAIL
+// *********************************************************
+// Task Distribution ( still to be planned )
+// Member_1:
+// Member_2:
+// Member_3:
+// Member_4:
+// *********************************************************
