95006ffdddfb0bd24882520273547d29074ad4d5
Hazim Zubair <hazimzubair81@gmail.com>
2025-12-27 02:22:42 +0800

    member1 ( somewhat completed Sheet & Column Setup)

--- Student Attendance Tracker.cpp
+++ Student Attendance Tracker.cpp
@@ -9,9 +9,55 @@
 // Member_3: ID | NAME | EMAIL
 // Member_4: ID | NAME | EMAIL
 // *********************************************************
-// Task Distribution ( still to be planned )
-// Member_1:
+// Task Distribution
+// Member_1: Sheet & Column Setup – Ask for sheet name, define columns, store column names & types
 // Member_2:
 // Member_3:
 // Member_4:
 // *********************************************************
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // VARIABLES
+    string SheetName, ColName[10], ColType[10], TypeChecker;
+    int NumOfCol, ColCount = 1;
+
+    cout << "STUDENT ATTENDANCE TRACKER - MILESTONE 1" << endl;
+    cout << "Enter attendance sheet name: ";
+    getline(cin, SheetName);
+
+    cout << "Attendance sheet " << SheetName << " created successfully." << endl;
+
+    cout << "Define number of columns (max 10): ";
+    cin >> NumOfCol;
+
+    cout << "Define " << NumOfCol << " columns." << endl;
+
+    while (NumOfCol != 0)
+    {
+        cout << "Enter column" << ColCount << " name: ";
+        cin >> ColName[ColCount];
+        while (true)
+        {
+            cout << "Enter column" << ColCount << "type (TEXT/INT): ";
+            cin >> TypeChecker;
+            if (TypeChecker == "TEXT" || TypeChecker == "INT")
+            {
+                ColType[ColCount] = TypeChecker;
+                cout << "Column" << ColCount << " \"" << ColName[ColCount] << "\" of type " << ColType[ColCount] << " created successfully." << endl;
+                break;
+            }
+            else
+            {
+                cout << "Invalid column type. Please enter either TEXT or INT." << endl;
+            }
+        }
+
+        ColCount++;
+        NumOfCol--;
+    }
+
+    return 0;
+}
